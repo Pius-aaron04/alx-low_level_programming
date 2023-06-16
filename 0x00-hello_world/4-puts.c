19a4b16837f83f61de18e201a63721b74c97d884
Pius Aaron <piuschbz@gmail.com>
2023-06-15 17:39:22 -0700

    C programming

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,10 @@
 #include <stdio.h>
-int main()
+/**
+ * main - Prints a message
+ * Return: returns 0 if program is successful
+ */
+int main(void)
 {
-	puts("Programming is like building a multilingual puzzle");
-	return 0;
+	puts("\"Programming is like building a multilingual puzzle\n");
+	return (0);
 }
