71889f9e8243dd8eae83c79ec715eb8f83c3af3a
Pius Aaron <piuschbz@gmail.com>
2023-06-16 18:32:44 -0700

    Base 10 digits

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - print 1 through 9
+ * Return: returns 0 on success
+ */
+int main(void)
+{
+	int x;
+
+	for (x = 0; x != 10; x++)
+		printf("%d", x);
+	printf("\n");
+	return (0);
+}
