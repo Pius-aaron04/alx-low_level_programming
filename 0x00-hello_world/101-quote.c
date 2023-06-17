f5210ae9555c1523561c9fdcbf4793b5d40d639f
Pius Aaron <piuschbz@gmail.com>
2023-06-17 03:58:30 -0700

    Quotes are useful

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - prints message to standard error
+ * Return: returns 1 on success
+ */
+int main(void)
+{
+	fprintf(stderr, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+	return (1);
+}
