b1aba76376c91159ca0c24dd4c7cec471b185097
Pius Aaron <piuschbz@gmail.com>
2023-06-20 13:26:58 -0700

    Revert "Alphabet"
    This reverts commit 37f33600462dfe680c2350d2bbb8abb3a10d972c.
    
    Printed alphabet more than required

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -6,12 +6,10 @@
 void print_alphabet_x10(void)
 {
 	int a = 0;
-	char s;
 
 	while (a <= 10)
 	{
-		for (s = 'a'; s <= 'z'; s++)
-			_putchar(s);
-		_putchar('\n');
+		print_alphabet();
+		a++;
 	}
 }
