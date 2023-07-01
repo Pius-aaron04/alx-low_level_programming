7bebc985013e96bdb7a445aa9992d89d032efe68
Pius Aaron <piuschbz@gmail.com>
2023-07-01 13:00:34 -0700

    Added another loop as required

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -6,21 +6,30 @@
 
 char *leet(char *str)
 {
+	int l = 0;
+	int x;
 	char *c = str;
 
-	for (; *str != '\0'; str++)
+	while (*str != 0)
 	{
-		if (*str == 'a' || *str == 'A')
-			*str = '4';
-
-		if (*str == 'e' || *str == 'E')
-			*str = '3';
-		if (*str == 'o' || *str == 'O')
-			*str = '0';
-		if (*str == 't' || *str == 'T')
-			*str = '7';
-		if (*str == 'l' || *str == 'L')
-			*str = '1';
+		str++;
+		l++;
 	}
-	return (c);
+	str = c;
+
+	for (x = 0; x <= l; x++)
+	{
+		if (str[x] == 'a' || str[x] == 'A')
+			str[x] = '4';
+
+		if (str[x] == 'e' || str[x] == 'E')
+			str[x] = '3';
+		if (str[x] == 'o' || str[x] == 'O')
+			str[x] = '0';
+		if (str[x] == 't' || str[x] == 'T')
+			str[x] = '7';
+		if (str[x] == 'l' || str[x] == 'L')
+			str[x] = '1';
+	}
+	return (str);
 }
