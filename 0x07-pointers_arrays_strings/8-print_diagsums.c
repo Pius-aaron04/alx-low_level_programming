fe36dd41b2e49f51521565ef0e5c819036398fe2
Pius Aaron <piuschbz@gmail.com>
2023-07-03 16:52:26 -0700

    Betty styling

--- 0x07-pointers_arrays_strings/8-print_diagsums.c
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -12,8 +12,8 @@ void print_diagsums(int *a, int size)
 	int sum2 = 0;
 
 	for (i = 0; i < size; i++)
-			sum1 += a[i][i];
+		sum1 += a[i * size + 1];
 	for (i = 0; i < size; i++)
-			sum2 += a[i][size - 1 - i];
+		sum2 += a[i * size + (size - 1 - i)];
 	printf("%d, %d\n", sum1, sum2);
 }
