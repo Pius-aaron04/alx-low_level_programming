3fe0da386bd884bb54f27f059e9d747f25264abe
Pius Aaron <piuschbz@gmail.com>
2023-07-03 16:11:06 -0700

    Find a match

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -16,5 +16,5 @@ char *_strchr(char *s, char c)
 			return (temp);
 		temp++;
 	}
-	return (NULL);
+	return (((void *)0));
 }
---
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,17 @@
+/**
+ * _strstr - located the first matching substring of accept in str
+ * @haystack: string to be searched
+ * @needle: string to match
+ * Return: pointer to the first character of the matched string
+ */
+
+char *_strstr(char *haystack, char *needle)
+{
+	while (*haystack != 0)
+	{
+		if ((*haystack == *needle) && *(haystack + 1) == *(needle + 1))
+			return (haystack);
+		haystack++;
+	}
+	return (((void *) 0));
+}
