0fb85096a151501f5454890ca73f99d8907f67b0
Pius Aaron <piuschbz@gmail.com>
2023-07-03 14:33:39 -0700

    Signatures

--- 0x07-pointers_arrays_strings/8-print_diagsums.c
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -12,8 +12,8 @@ void print_diagsums(int *a, int size)
 	int sum2 = 0;
 
 	for (i = 0; i < size; i++)
-			sum1 += a[i * size + i];
+			sum1 += a[i][i];
 	for (i = 0; i < size; i++)
-			sum2 += a[i * size + (size - i - 1)];
+			sum2 += a[i][size - 1 - i];
 	printf("%d, %d\n", sum1, sum2);
 }
