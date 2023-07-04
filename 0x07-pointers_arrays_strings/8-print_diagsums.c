707533e233ab4e11a77dcc6d377a38c94bf5f886
Pius Aaron <piuschbz@gmail.com>
2023-07-03 17:06:31 -0700

    C is all the way fun

--- 0x07-pointers_arrays_strings/8-print_diagsums.c
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -12,7 +12,7 @@ void print_diagsums(int *a, int size)
 	int sum2 = 0;
 
 	for (i = 0; i < size; i++)
-		sum1 += a[i * size + 1];
+		sum1 += a[i * size + i];
 	for (i = 0; i < size; i++)
 		sum2 += a[i * size + (size - 1 - i)];
 	printf("%d, %d\n", sum1, sum2);
