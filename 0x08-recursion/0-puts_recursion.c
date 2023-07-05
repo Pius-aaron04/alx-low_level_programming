5e464030f81f25ad7595b9567bd8d2585247c080
Pius Aaron <piuschbz@gmail.com>
2023-07-05 05:12:50 -0700

    Prints recursively

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * _puts_recursion - recursively prints char of a string
+ * @s: string
+ */
+
+void _puts_recursion(char *s)
+{
+	if (!(*s))
+	{
+		_putchar('\n');
+		return;
+	}
+	else
+	{
+		_putchar(*s);
+		++s;
+		_puts_recursion((s));
+	}
+}
---
+++ 0x08-recursion/README.md
@@ -0,0 +1,1 @@
+Deeper and deeper we go.
