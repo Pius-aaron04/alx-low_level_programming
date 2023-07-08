1c9af88d49f3224b1b1560092a28926cb910c229
Pius Aaron <piuschbz@gmail.com>
2023-07-08 07:32:38 -0700

    Multiply at command line

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - multplies two numbers from standard input
+ * @argc: number of arguments
+ * @argv: array of parameters
+ * Return: 0 on success and 1 0n failure
+ */
+
+int main(int argc, char *argv[3])
+{
+	int product = 1;
+	int i;
+
+	for (i = 1; i < argc; i++)
+	{
+		if (*argv[i] >= '0' || *argv[i] <= '9')
+		{
+			product *= atoi(argv[i]);
+		}
+		else
+		{
+			printf("Error");
+			return (1);
+		}
+	}
+	printf("%d\n", product);
+	return (0);
+}
