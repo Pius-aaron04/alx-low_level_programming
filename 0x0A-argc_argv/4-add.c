03c94f9fdb336c469e0fce35bd12d555134f2f47
Pius Aaron <piuschbz@gmail.com>
2023-07-07 17:16:21 -0700

    Add at command line

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,31 @@
+#include <stdlib.h>
+#include <stdio.h>
+/**
+ * main - adds positive numbers
+ * @argc: number of arguments
+ * @argv: array of integers
+ * Return: 0 on succes and 1 on failure
+ */
+int main(int argc, char *argv[])
+{
+	int sum = 0;
+	int i;
+
+	if (argc == 1)
+		printf("0\n");
+	else if (argc > 1)
+	{
+		for (i = 1; i < argc; i++)
+		{
+			if (*argv[i] >= '0' && *argv[i] <= '9')
+				sum += atoi(argv[i]);
+			else
+			{
+				printf("Error");
+				return(1);
+			}
+		}
+		printf("%d\n", sum);
+	}
+	return (0);
+}
