20d4aa7703aa52a819413df17268073678f88f6d
Pius Aaron <piuschbz@gmail.com>
2023-07-10 13:36:34 -0700

    task 1

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,31 @@
+#include <stdlib.h>
+
+/**
+ * create_array - Creates an array of n elements
+ * @size: number of elements or size of array
+ * @c: character to occupy array
+ * Return: a char pointer
+ */
+
+char *create_array(unsigned int size, char c)
+{
+	char *array;
+	unsigned int i = 0;
+
+	if (size == 0)
+		return (NULL);
+	array = (char *)malloc(sizeof(char) * size);
+
+	while (1)
+	{
+		if (i != size)
+			array[i] = c;
+		else
+		{
+			array[i] = '\0';
+			break;
+		}
+		i++;
+	}
+	return (array);
+}
---
+++ 0x0B-malloc_free/README.md
@@ -0,0 +1,2 @@
+This directory contains Dynamic Memory Allocation Projects.
+Allocating and deallocating memories and fixing memory leaks.
