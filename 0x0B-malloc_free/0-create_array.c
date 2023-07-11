b1ae15cc99eb638a7551a563fb795a4c0e714482
Pius Aaron <piuschbz@gmail.com>
2023-07-11 00:11:48 -0700

    Finally

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -13,7 +13,7 @@ char *create_array(unsigned int size, char c)
 	char *array;
 	unsigned int i = 0;
 
-	if (size == 0 || size == INT_MAX)
+	if (size == 0 || size == CHAR_MAX)
 		return (NULL);
 	array = (char *)malloc(sizeof(char) * size);
 
