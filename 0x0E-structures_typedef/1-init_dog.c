7b9386836671f6844331ad5bf3e549c96db83533
Pius Aaron <piuschbz@gmail.com>
2023-07-17 14:37:44 -0700

    Betty flagged

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -8,7 +8,7 @@
  * Return: Nothing
  */
 
-void init_dog(struct dog *d, char *name , float age, char *owner)
+void init_dog(struct dog *d, char *name, float age, char *owner)
 {
 	(*d).name = name;
 	(*d).age = age;
