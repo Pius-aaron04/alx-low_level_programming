4a30e96691b4c024ca34140a55892eb877ca6cad
Pius Aaron <piuschbz@gmail.com>
2023-07-17 04:39:18 -0700

    C preprocessing

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main - prints file name where
+ * Return: 0 on success otherwise 1
+ */
+
+int main(void)
+{
+	printf("%s\n", __BASE_FILE__);
+	return (0);
+}
---
+++ 0x0D-preprocessor/3-function_like_macro.h
@@ -0,0 +1,4 @@
+#ifndef _FILE_NAME_H
+#define _FILE_NAME_H
+#define ABS(x) ((x > 0) ? (x) : (-1) * (x))
+#endif
