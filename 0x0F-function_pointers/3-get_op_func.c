fc414c03ca04681bd2a9837db248e30184240b5d
Pius Aaron <piuschbz@gmail.com>
2023-07-19 23:57:00 +0000

    Keep trying

--- 0x0F-function_pointers/3-get_op_func.c
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -14,11 +14,10 @@ int (*get_op_func(char *s))(int a, int b)
 		{"*", op_mul},
 		{"/", op_div},
 		{"%", op_mod},
-		{NULL, NULL},
 	};
 	int i = 0;
 
-	while (i < 6)
+	while (i < 5)
 	{
 		if (*s == *(ops[i].symbol))
 			return (ops[i].op);
