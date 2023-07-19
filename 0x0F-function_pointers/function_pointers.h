00d2a4480edb58e756ae2c24e98a100ddaf3f2d2
Pius Aaron <piuschbz@gmail.com>
2023-07-19 14:02:25 +0000

    double includeion guard

--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -1,6 +1,6 @@
 #ifndef _FILE_NAME_H
-#include <stddef.h>
 #define _FILE_NAME_H
+#include <stddef.h>
 void print_name(char *name, void (*f)(char *));
 void array_iterator(int *array, size_t size, void (*action)(int));
 void array_iterator(int *array, size_t size, void (*action)(int));
