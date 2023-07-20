13edb64d00c727b06e6076ca49be39f47f6ae61d
Pius Aaron <piuschbz@gmail.com>
2023-07-20 11:05:30 +0000

    6 checks failed

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -16,7 +16,7 @@ int main(int argc, char *argv[])
 	if (argc != 4)
 	{
 		printf("Error\n");
-		return (1);
+		exit(98);
 	}
 	/* converts strings */
 	num1 = atoi(argv[1]);
@@ -32,5 +32,5 @@ int main(int argc, char *argv[])
 	}
 
 	printf("Error\n");
-	return (1);
+	return (99);
 }
