602d48ca0ee417673aea722a734441cc05611f67
Pius Aaron <piuschbz@gmail.com>
2023-07-27 19:44:13 +0000

    task 2 and 3 corrected

--- 0x12-singly_linked_lists/3-add_node_end.c
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -17,18 +17,21 @@ list_t *add_node_end(list_t **head, const char *str)
 	{
 		return (NULL);
 	}
+	new_node->str = strdup(str);
+	new_node->len = strlen(new_node->str);
+	new_node->next = NULL;
+	if (*head == NULL)
+	{
+		*head = new_node;
+	}
 	else
 	{
-		new_node->str = strdup(str);
-		new_node->len = strlen(new_node->str);
-		new_node->next = NULL;
 		current_node = *head;
-
-		while (current_node != NULL)
+		while (current_node->next != NULL)
 		{
 			current_node = current_node->next;
 		}
-		current_node = new_node;
+		current_node->next = new_node;
 	}
 	return (*head);
 }
