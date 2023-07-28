2b2271f9ab015090f35b745ba1af61af737d0898
Pius Aaron <piuschbz@gmail.com>
2023-07-28 20:15:01 +0000

    Hola

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -12,7 +12,7 @@ list_t *add_node(list_t **head, const char *str)
 {
 	list_t *new = malloc(sizeof(list_t));
 
-	if (new == NULL || (strdup(str)) == NULL)
+	if (new == NULL)
 		return (NULL);
 	new->str = strdup(str);
 	if (new->str == NULL)
