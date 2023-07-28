b9c3bce95e4d1d96fa806c6405eea07e258b4833
Pius Aaron <piuschbz@gmail.com>
2023-07-28 20:13:41 +0000

    Hola

--- 0x12-singly_linked_lists/3-add_node_end.c
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -19,7 +19,7 @@ list_t *add_node_end(list_t **head, const char *str)
 	}
 	new_node->str = strdup(str);
 	if (new_node->str == NULL)
-		new_str->str == "(nil)";
+		new_node->str = "(nil)";
 	new_node->len = strlen(new_node->str);
 	new_node->next = NULL;
 	if (*head == NULL)
