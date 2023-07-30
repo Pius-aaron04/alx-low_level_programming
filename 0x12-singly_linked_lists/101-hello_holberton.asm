7ab3126ab5b01ef3d9008fa7ff523cbf60c74f08
Pius Aaron <piuschbz@gmail.com>
2023-07-30 14:46:42 +0000

    Assembly language

--- 0x12-singly_linked_lists/101-hello_holberton.asm
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -1,45 +1,17 @@
-	.file	"101-hello_holberton.c"
-	.text
-	.section	.rodata
-.LC0:
-	.string	"Hello Holberton"
-	.text
-	.globl	main
-	.type	main, @function
-main:
-.LFB0:
-	.cfi_startproc
-	endbr64
-	pushq	%rbp
-	.cfi_def_cfa_offset 16
-	.cfi_offset 6, -16
-	movq	%rsp, %rbp
-	.cfi_def_cfa_register 6
-	leaq	.LC0(%rip), %rdi
-	movl	$0, %eax
-	call	printf@PLT
-	movl	$0, %eax
-	popq	%rbp
-	.cfi_def_cfa 7, 8
-	ret
-	.cfi_endproc
-.LFE0:
-	.size	main, .-main
-	.ident	"GCC: (Ubuntu 9.4.0-1ubuntu1~20.04.1) 9.4.0"
-	.section	.note.GNU-stack,"",@progbits
-	.section	.note.gnu.property,"a"
-	.align 8
-	.long	 1f - 0f
-	.long	 4f - 1f
-	.long	 5
-0:
-	.string	 "GNU"
-1:
-	.align 8
-	.long	 0xc0000002
-	.long	 3f - 2f
-2:
-	.long	 0x3
-3:
-	.align 8
-4:
+section .data	;data section
+
+hello db 'Hello, Holberton\n', 0
+
+section .text	; code section
+	global _start ; entry point
+_start:
+	mov eax, 4	; system call number for sys_write
+	mov ebx, 1	; file descriptor (stdout)
+	mov ecx, hello	; string data
+	mov edx, 17	; string length
+	int 0x80	; Interrupt to invoke function call
+
+	; Program exit
+	mov eax, 1	; system call number for sys_exit (1)
+	xor ebx, ebx	; exit status (0)
+	int 0x80	; interrupts for system calls
