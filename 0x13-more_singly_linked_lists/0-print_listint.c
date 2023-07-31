b31898e7575f8cbe1ee389b4365d644acfb1656e
Pius Aaron <piuschbz@gmail.com>
2023-07-31 11:02:39 +0000

    Task 0

---
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -0,0 +1,23 @@
+#include "lists.h"
+
+/**
+ * print_listint - prints int value in list
+ * @h: list head
+ * 
+ * Return: number of nodes
+ */
+
+
+size_t print_listint(const listint_t *h)
+{
+	size_t nodes = 0;
+	listint_t *current = (listint_t *)h;
+
+	while (current != NULL)
+	{
+		printf("%d\n", current->n);
+		nodes++;
+		current = current->next;
+	}
+	return (nodes);
+}
