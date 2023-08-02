63171bc16feac0a2ddf66f6dafc5526584fc3ee9
Pius Aaron <piuschbz@gmail.com>
2023-08-02 08:38:58 +0000

    NULL handled

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -10,7 +10,7 @@ void free_listint2(listint_t **head)
 	listint_t *temp;
 	listint_t *current;
 
-	if (*head != NULL)
+	if (head && *head)
 	{
 		if ((*head)->next != NULL)
 		{
@@ -31,5 +31,4 @@ void free_listint2(listint_t **head)
 			*head = NULL;
 		}
 	}
-	return (NULL);
 }
