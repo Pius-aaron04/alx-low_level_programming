eddd0202fdd29e35c0708eaaa8bd6bceacaa43f7
Pius Aaron <piuschbz@gmail.com>
2023-08-05 08:06:38 +0000

    main.h

--- 0x14-bit_manipulation/main.h
+++ 0x14-bit_manipulation/main.h
@@ -7,4 +7,5 @@ int get_bit(unsigned long int n, unsigned int index);
 void print_binary(unsigned long int n);
 unsigned int flip_bits(unsigned long int n, unsigned long int m);
 int _putchar(char c);
+int get_endianness(void);
 #endif
