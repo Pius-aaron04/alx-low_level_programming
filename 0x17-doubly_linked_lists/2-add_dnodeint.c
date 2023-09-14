9416359cf0936d9920be5bf3382403b867e45314
Aaron Pius <piuschbz@gmail.com>
2023-09-14 14:21:00 +0000

    Add new node

--- 0x17-doubly_linked_lists/2-add_dnodeint.c
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -14,19 +14,12 @@ dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 
 	if (new_node == NULL)
 		return NULL;
-	new_node->next = NULL;
 	new_node->prev = NULL;
 	new_node->n = (int)n;
 
-	if (*head == NULL)
-	{
-		printf("New node with data: %d added\n", new_node->n);
-		return (new_node);
-	}
-	else
-	{
-		new_node->next = *head;
+	new_node->next = *head;
+	if (*head != NULL)
 		(*head)->prev = new_node;
-	}
+	*head = new_node;
 	return (*head);
 }
