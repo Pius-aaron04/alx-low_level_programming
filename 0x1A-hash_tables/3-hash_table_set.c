2f0ac3bdf7e6cc84f89e4f7c04b68204c164e06c
Aaron Pius <piuschbz@gmail.com>
2023-10-06 15:36:38 +0000

    task 3

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -14,7 +14,7 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 
 	new_node = (hash_node_t *)malloc(sizeof(hash_node_t));
 
-	if (strlen(key) == 0)
+	if (strlen(key) == 0 || !key || !value)
 		return (0);
 	if (new_node == NULL)
 		return (0);
