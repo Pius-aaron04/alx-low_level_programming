409606abbd87b5e54566f15f23005c700e967eff
Aaron Pius <piuschbz@gmail.com>
2023-10-06 21:02:41 +0000

    print hash table

---
+++ 0x1A-hash_tables/5-hash_table_print.c
@@ -0,0 +1,35 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_print - print data in hash table
+ * @ht: hash_table
+ */
+
+void hash_table_print(const hash_table_t *ht)
+{
+	hash_node_t *pair;
+	unsigned long int i;
+	int first_element = 0;
+
+	if (ht == NULL)
+	{
+		printf("{}\n");
+		return;
+	}
+
+	putchar('{');
+	for (i = 0; i < ht->size; i++)
+	{
+		pair = ht->array[i];
+		while (pair)
+		{
+			if (first_element)
+				printf(", ");
+			else
+				first_element = 1;
+			printf("'%s': '%s'", pair->key, pair->value);
+			pair = pair->next;
+		}
+	}
+	puts("}");
+}
