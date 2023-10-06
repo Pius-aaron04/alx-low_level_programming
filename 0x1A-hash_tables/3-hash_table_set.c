6a8b9876238565d0b97e1024a49fd93775491c1a
Aaron Pius <piuschbz@gmail.com>
2023-10-06 14:58:36 +0000

    task 3

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -26,11 +26,12 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 		return (0);
 	index = key_index((const unsigned char *)key, ht->size);
 
-	if (!ht->array[index])
+	if (ht->array[index] == NULL)
 		ht->array[index] = new_node;
 	else
 	{
 		new_node->next = ht->array[index];
+		ht->array = new_node;
 	}
 	return (1);
 }
