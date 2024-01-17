ae98de57c723654c260c0613f9936caad9eb7c48
Pius Aaron <piuschbz@gmail.com>
2024-01-17 14:58:37 +0100

    add: interpolation search

---
+++ 0x1E-search_algorithms/102-interpolation.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * interpolation_search - seacrhes an array of integers
+ * @array: pointer to the first element in the array
+ * @size: length/size of the array
+ * @value: value to search for
+ * Return: index of the value if found else -1
+ */
+
+
+int interpolation_search(int *array, size_t size, int value)
+{
+	size_t lb, hb, pos;
+
+	hb = size - 1;
+	lb = 0;
+
+	while (lb <= hb)
+	{
+		pos = lb + (((double)(hb - lb) / (array[hb] - array[lb])) *
+				(value - array[lb]));
+		if (pos > size - 1)
+		{
+			printf("Value checked array[%ld] is out of range\n", pos);
+			return (-1);
+		}
+		printf("Value checked array[%ld] = [%d]\n", pos, array[pos]);
+		if (array[pos] == value)
+			return (pos);
+		if (value < array[pos])
+			hb = pos;
+		else
+			lb = pos;
+
+	}
+	return (-1);
+}
